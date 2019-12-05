edec5321800e02d5e27796f9febedc9d5003c73b
Pierre Darrieutort <p.darrieutort@outlook.fr>
2019-12-05 14:20:34 +0100

    initial commit

---
+++ src/App copy.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import React, { Component } from 'react'
+
+class App extends Component {
+
+
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      data: ''
+    }
+  }
+
+
+  componentDidMount() {
+    fetch( 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/908622995' )
+      .then( ( response ) => response.json() )
+      .then( ( response ) => {
+        const a = response.tracks.data[1].preview
+        this.setState( {
+          data: a
+        } )
+        console.log( a )
+      } )
+  }
+
+
+  render() {
+    const {data} = this.state
+    return (
+    <div>{data}</div>
+    )
+  }
+}
+
+export default App
