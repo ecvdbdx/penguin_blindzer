d1bc63e3ffcfb1e614e2657aa12790af8792cc32
Pierre Darrieutort <p.darrieutort@outlook.fr>
2019-12-12 18:49:00 +0100

    wip endGame

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,12 @@ class App extends Component {
       answer: '',
       time: 0,
       score: 0,
-      history: []
+      history: [],
+      endGame: false
     }
 
     this.reward = this.reward.bind( this )
+    this.renderEndGame = this.renderEndGame.bind( this )
     this.getCurrentTime = this.getCurrentTime.bind( this )
     this.randSong = this.randSong.bind( this )
     this.getInputValue = this.getInputValue.bind( this )
@@ -50,12 +52,13 @@ class App extends Component {
       const songTitle = this.state.song.title.toLowerCase().replace( /(\((.*?)\))/, '' ).trim()
       const answer = this.state.answer.toLowerCase()
       const match = stringSimilarity.compareTwoStrings( songTitle, answer )
-      console.log( match, songTitle, answer )
       if ( match >= .7 && songTitle.length === answer.length ) {
         this.state.history.push( songTitle )
         this.reward()
         console.log( this.state.history )
-        this.randSong()
+        if ( this.state.data === [] )
+          this.setState( { endGame: !this.state.endGame } )
+        else this.randSong()
       }
     } )
   }
@@ -70,15 +73,23 @@ class App extends Component {
     this.setState( { time: currentTime } )
   }
 
+ renderEndGame() {
+   return (
+     <h1>End Game</h1>
+   )
+ }
+
   render() {
-    const { song } = this.state
+    const { song, endGame } = this.state
     return (
       <>
-        <h1>{song.title}</h1>
-        <audio onEnded={this.randSong} onTimeUpdate={this.getCurrentTime} src={song.preview} controls autoPlay />
-        <section>
-          <input type="text" value={this.state.answer} autoFocus onChange={( e ) => this.getInputValue( e )} />
-        </section>
+        {endGame === false ? ( <>
+          <h1>{song.title}</h1>
+          <audio onEnded={this.randSong} onTimeUpdate={this.getCurrentTime} src={song.preview} controls autoPlay />
+          <section>
+            <input type="text" value={this.state.answer} autoFocus onChange={( e ) => this.getInputValue( e )} />
+          </section>
+        </> ) : this.renderEndGame()}
       </>
     )
   }
