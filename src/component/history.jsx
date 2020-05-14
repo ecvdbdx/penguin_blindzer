cd9ab6f1127b03630fa4db0cc9c1ca646b23d508
Anthony H <flohoa-a@outlook.com>
2020-05-14 11:27:51 +0200

    Add transition cards

--- src/component/history.jsx
+++ src/component/history.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Transition} from 'react-spring/renderprops'
 import HistoryCard from "./historyCard";
 
 const History = ({history}) =>{
@@ -7,13 +8,22 @@ const History = ({history}) =>{
         <div className="history-wrapper">
             {newHistory.length > 0 && (
                 newHistory.map((i, index) => (
-
-                    <HistoryCard
-                        key = {index}
-                        title = {i.title}
-                        artist = {i.artist}
-                        success = {i.success}
-                    />
+                    <Transition
+                    items={i} key={index}
+                    from={{ transform: 'translate3d(-250px,0,0)' }}
+                    enter={{ transform: 'translate3d(0px,0,0)' }}
+                    leave={{ transform: 'translate3d(-250px,0,0)' }}>
+                        {i => props =>
+                        <div key= {index} style= {props}>
+                            <HistoryCard
+                                key = {index}
+                                title = {i.title}
+                                artist = {i.artist}
+                                success = {i.success}
+                            />
+                        </div>
+                        }
+                    </Transition>
                 ))
             )
             }
